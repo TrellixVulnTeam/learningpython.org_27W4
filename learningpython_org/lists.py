1b0256a72bead87d9b58e4ce1f4ff695b7e61c43
Juliano <afro.juliano@gmail.com>
2022-07-19 02:33:32 -0300

    learning lists

--- learningpython_org/lists.py
+++ learningpython_org/lists.py
@@ -7,9 +7,31 @@ mylist.append(3)
 
 print(mylist[0])  # prints 1
 print(mylist[1])  # prints 2
-print(mylist[3])
+print(mylist[2])
 
 
 # prints out 1, 2, 3
 for x in mylist:
     print(x)
+
+""""
+mylist = [1, 2, 3]
+print(mylist[10])
+"""
+
+numbers = []
+strings = []
+names = ["Michele", "Adriane", "Juliano"]
+second_name = names[1]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings.append("hello")
+strings.append("world!")
+
+print(numbers)
+print(strings)
+print(names)
+print("The second name on the names list is %s" % second_name)
