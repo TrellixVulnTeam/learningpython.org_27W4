df87db4fc69edec4a2c07b1ac6deb717c4d9b8b3
Juliano <afro.juliano@gmail.com>
2022-07-19 02:11:56 -0300

    commit test

--- learningpython_org/helloworld.py
+++ learningpython_org/helloworld.py
@@ -1,8 +1,48 @@
+# hello
 hi = "Hello World!!!"
 print(hi)
 
+# operations variables
 x = 1
 if x == 1:
     print("x is 1.")
 
 print("Hello, World!")
+
+# integer and float
+myint = 7
+print(myint)
+
+myFloat = 7.0
+print(myFloat)
+myFloat = float(7)
+print(myFloat)
+
+# strings
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+a, b = 3, 4
+print(a, b)
+
+# this will not work!
+one = 1
+two = 2
+hello = "hello"
+
+# print(one + two +_hello)
