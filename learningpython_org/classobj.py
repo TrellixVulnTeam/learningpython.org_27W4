5700e0331130a586d3cc055aaedcd91ed335c985
Juliano <afro.juliano@gmail.com>
2022-07-28 21:49:42 -0300

    initiating classes and objects

---
+++ learningpython_org/classobj.py
@@ -0,0 +1,11 @@
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a menssage inside the class.")
+
+
+classobj = MyClass()
+
+print(classobj.variable)
+classobj.function()
