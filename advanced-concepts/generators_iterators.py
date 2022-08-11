0c4106659fabb46a0cf9fd1979d8eafb4f4defd0
Juliano <afro.juliano@gmail.com>
2022-08-10 23:57:54 -0300

    Working one more time with generators

--- advanced-concepts/generators_iterators.py
+++ advanced-concepts/generators_iterators.py
@@ -1,10 +1,12 @@
+import types
+"""
 import random
 
 
 def lottery():
     # returns 6 numbers between 1 and 40
     for i in range(6):
-        print(i)
+        # print(i)
         yield random.randint(1, 40)
 
     # returns a 7th number between 1 and 15
@@ -13,3 +15,24 @@ def lottery():
 
 for random_number in lottery():
     print("And the next number is... %d!" % (random_number))
+"""
+
+
+# fill in this function
+def fib():
+    a, b = 1, 1
+    while 1:
+        yield a
+        a, b = b, a + b
+
+
+# testing code
+if type(fib()) == types.GeneratorType:
+    print("Good, The fib function is a generator.")
+
+    counter = 0
+    for n in fib():
+        print(n)
+        counter += 1
+        if counter == 10:
+            break
